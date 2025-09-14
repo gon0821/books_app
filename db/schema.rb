a1015a4a319cc14c2a541ae3c6f4c7da59e2e36c
gon821 <gonty821@gmail.com>
2025-09-14 18:00:49 +0900

    add image upload function

--- db/schema.rb
+++ db/schema.rb
@@ -10,12 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.0].define(version: 2025_09_14_023723) do
+ActiveRecord::Schema[8.0].define(version: 2025_09_14_082809) do
   create_table "books", force: :cascade do |t|
     t.string "title"
     t.text "memo"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "author"
+    t.string "picture"
   end
 end
